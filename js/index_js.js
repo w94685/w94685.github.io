f269e0dea9ef6000db6e376b4c7fa2448a598cc1
ZH <w94685@gmail.com>
2017-05-04 10:15:26 -0500

    Add files via upload

--- js/index_js.js
+++ js/index_js.js
@@ -1,3 +1,4 @@
+
 $(document).ready(function(e) {
 	//welcome page----------------------------------
 	$(".main").animate({'opacity':1,'top':'15vh'},2000);
@@ -8,21 +9,65 @@ $(document).ready(function(e) {
 	});
 
 	//change page-----------------------------------
-	var cur = 0;
-	var next;
+	var main_cur = 0;
+	var main_next;
 
 	$(".nav li").each(function(index){
 		$(this).click(function(){
-			next = index;
+			main_next = index;
 			changePage();
 		});
 	});
 
 	function changePage(){
-		if(next != cur){
-			$(".textbox").eq(next).stop().animate({'opacity':1,'z-index':10},700);
-			$(".textbox").eq(cur).stop().animate({'opacity':0,'z-index':1},1000);
+		if(main_next != main_cur){
+			$(".textbox").eq(main_next).stop().animate({'opacity':1,'z-index':2},700);
+			$(".textbox").eq(main_cur).stop().animate({'opacity':0,'z-index':1},1000);
 		};
-		cur = next;
+		main_cur = main_next;
+	};
+
+	//Page3 slide-------------------------------------
+	var slide_interval;
+	var slide_cur = 0;
+	var slide_next;
+
+	$("#right").click(function(){
+		slide_next = slide_cur + 1;
+		slide();
+	});
+	$("#left").click(function(){
+		slide_next = slide_cur - 1;
+		slide();
+	});
+	$(".smallbtn").each(function(index){
+		$(this).click(function(){
+			if(slide_cur != index){
+				slide_next = index;
+				slide();
+			};
+		});
+	});
+
+	slideON();
+	function slideON(){
+		slide_next = slide_cur + 1;
+		slide_interval = setInterval(slide,5000);
+	};
+	function slideOFF(){
+		clearInterval(slide_interval);
+	};
+	function slide(){
+		slideOFF();
+		if(slide_next > 2) slide_next = 0;
+		if(slide_next < 0) slide_next = 2;
+
+		if(slide_next != slide_cur){
+			slide_cur = slide_next;
+			var slide_width = $(".slide_box").width();
+			$(".slide").stop().animate({'left': -slide_width * slide_next},1000,function(){
+				slideON();
+			});
+		};
 	};
 });
